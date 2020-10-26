55665d4f4d39973f0b2fd94183805f8fc7a4ca37
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-10-26 16:41:34 +0300

    Add files via upload

---
+++ KapsüllemeÖrnek/Ev.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsüllemeÖrnek
+{
+    class Ev
+    {
+        private string semt;
+        public string Semt
+        {
+            get { return semt; }
+
+            set { semt = value.ToUpper(); }
+        }
+
+        private int odasayisi;
+        public int OdaSayisi
+        {
+            get { return odasayisi; }
+            set { odasayisi = Math.Abs(value); }
+        }
+
+        private int kat;
+        public int Kat
+        {
+            get { return kat; }
+            set { kat = Math.Abs(value); }
+        }
+
+        private int alan;
+        public int Alan
+        {
+            get { return alan; }
+            set { alan = Math.Abs(value); }
+        }
+
+
+    }
+}
---
+++ KapsüllemeÖrnek/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KapsüllemeÖrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ev evim = new Ev();
+            evim.Semt = textBox1.Text;
+            evim.OdaSayisi = Convert.ToInt32(textBox2.Text);
+            evim.Kat = Convert.ToInt32(textBox3.Text);
+            evim.Alan = Convert.ToInt32(textBox4.Text);
+
+            ListViewItem ekle = new ListViewItem();
+            ekle.Text = evim.Semt;
+            ekle.SubItems.Add(evim.OdaSayisi.ToString());
+            ekle.SubItems.Add(evim.Kat.ToString());
+            ekle.SubItems.Add(evim.Alan.ToString());
+
+            listView1.Items.Add(ekle);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+    }
+}
